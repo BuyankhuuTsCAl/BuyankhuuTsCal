f4b3d961aab6bea119faea1186c835f8e31e4899
Buyankhuuts <65098276+Buyankhuuts@users.noreply.github.com>
2022-11-07 19:15:57 -0800

    hw8

---
+++ homeworks/hw8/assets/js/script.js
@@ -0,0 +1,6 @@
+document.getElementById("toggle-button").onclick = toggleSongs;
+function toggleSongs(){
+    second_Song = document.getElementById("songs-second-half");
+    second_Song.classList.add("hidden");
+
+}
